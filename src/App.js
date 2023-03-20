46e742a2bbc768f81442fd03710e6519673b1673
sasw <saswain96@gmail.com>
2023-03-20 13:19:33 -0500

    updated browserrouter basename

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/portfolio'>
       <Navbar />
       <Routes>
         <Route path="/" element={<About />} />
