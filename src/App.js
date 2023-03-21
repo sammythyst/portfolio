e5a41ad73da548e32f197ab985d8fb9e618ac2a9
sasw <saswain96@gmail.com>
2023-03-21 13:02:40 -0500

    changed base routes for custom domain

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename='/portfolio'>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<About />} />
