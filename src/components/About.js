b6c7ff3a79d51a861a1c4f65e26e4aab559989c3
sasw <saswain96@gmail.com>
2023-03-21 15:18:26 -0500

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,6 @@ function About() {
                 <div>
                     <h3>Sam is a Web Developer with technical education from Purdue University Northwest and Michigan State University.</h3>
                     <p>They enrolled in the Computer Graphics Technology program at Purdue in the fall of 2016, graduating with a Bachelor's degree in May 2020. Then in the summer of 2022, they decided to further their coding education and enrolled in a Full Stack Coding Boot Camp through Michigan State, graduating from this program in March 2023.</p>
-                    <p>Suspendisse malesuada semper lorem, sit amet feugiat sapien congue ut. Vestibulum accumsan mattis sapien, ut pulvinar arcu hendrerit non. Nulla libero tellus, luctus ut pharetra malesuada, semper nec lacus. Maecenas tristique, nulla a pellentesque ornare, mauris magna venenatis ipsum, sit amet mollis lacus turpis nec felis.</p>
                 </div>
                 <div>
                     <img src={require('../images/avatar.png')} alt="Cartoon image of the developer." style={dev}/>
