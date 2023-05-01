ec26aa53605960e1cd80d701aa146bedc8cf0e02
sasw <saswain96@gmail.com>
2023-04-30 22:10:26 -0500

    updated images for efficiency

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,9 +17,7 @@ function Portfolio() {
 
     const project = {
         maxWidth: "450px",
-        // margin: "auto",
-        padding: "30px",
-        // border: "1px solid black"
+        padding: "30px"
     }
 
 
@@ -33,7 +31,7 @@ function Portfolio() {
                     </div>
                     <p>A microblog for artists and art lovers to share, view, and appreciate art.</p>
                     <p>Tools: Node.js, Express.js, Handlebars.js, MySQL</p>
-                    <img src={require('../images/media-phile.png')} alt="media phile homepage with a couple posts from various users." style={img}/>
+                    <img src={require('../images/media-phile.jpg')} alt="media phile homepage with a couple posts from various users." style={img}/>
                 </div>
                 <div style={project}>
                     <div style={links}>
@@ -42,7 +40,7 @@ function Portfolio() {
                     </div>
                     <p>Find information on universities and colleges by name and discover local weather data.</p>
                     <p>Tools: HTML, CSS, JavaScript, Bootstrap 5, jQuery, APIs</p>
-                    <img src={require('../images/college-trip.png')} alt="webpage listing various colleges based on search term" style={img}/>
+                    <img src={require('../images/college-trip.jpg')} alt="webpage listing various colleges based on search term" style={img}/>
                 </div>
                 <div style={project}>
                     <div style={links}>
@@ -51,7 +49,7 @@ function Portfolio() {
                     </div>
                     <p>A database website collecting known trading cards from the YouTube musical group, Random Encounters.</p>
                     <p>Tools: HTML, CSS, JavaScript, JSON</p>
-                    <img src={require('../images/re-tcg.png')} alt="pokemon-like cards on a red and black starburst background" style={img}/>
+                    <img src={require('../images/re-tcg.jpg')} alt="pokemon-like cards on a red and black starburst background" style={img}/>
                 </div>
                 {/* module 18 */}
                 <div style={project}>
@@ -61,7 +59,7 @@ function Portfolio() {
                     </div>
                     <p>Backend of a social network directory where users can create new accounts, find and add friends, and share and react to thoughts.</p>
                     <p>Tools: Express.js, Mongoose, Moment</p>
-                    <img src={require('../images/social-network-api.png')} alt="backend view of a social network's api" style={img}/>
+                    <img src={require('../images/social-network-api.jpg')} alt="backend view of a social network's api" style={img}/>
                 </div>
                 {/* module 13 */}
                 <div style={project}>
@@ -71,7 +69,7 @@ function Portfolio() {
                     </div>
                     <p>E-commerce database displaying various products and categories belonging to the store.</p>
                     <p>Tools: Express.js, MySQL2, Sequelize, Dotenv</p>
-                    <img src={require('../images/ecommerce-backend.png')} alt="backend view of ecommerce store" style={img}/>
+                    <img src={require('../images/ecommerce-backend.jpg')} alt="backend view of ecommerce store" style={img}/>
                 </div>
                 {/* module 12 */}
                 <div style={project}>
@@ -81,7 +79,7 @@ function Portfolio() {
                     </div>
                     <p>A command line application to view, add, and update the departments, employees, and employee roles within a company.</p>
                     <p>Tools: Node.js, Inquirer, MySQL</p>
-                    <img src={require('../images/employee-tracker.png')} alt="a command line application of a company database" style={img}/>
+                    <img src={require('../images/employee-tracker.jpg')} alt="a command line application of a company database" style={img}/>
                 </div>
                  {/* module 10 */}
                 <div style={project}>
@@ -91,7 +89,7 @@ function Portfolio() {
                     </div>
                     <p>Terminal application to allow teams to add employees based on their position.</p>
                     <p>Tools: Inquirer</p>
-                    <img src={require('../images/team-profile.png')} alt="cards displaying names and information of various employees" style={img}/>
+                    <img src={require('../images/team-profile.jpg')} alt="cards displaying names and information of various employees" style={img}/>
                 </div>
                 {/* module 6 */}
                 <div style={project}>
@@ -101,7 +99,7 @@ function Portfolio() {
                     </div>
                     <p>Search for a city and discover its weather forecast.</p>
                     <p>Tools: HTML, CSS, JavaScript, API</p>
-                    <img src={require('../images/weather-dashboard.png')} alt="a weather dashboard for Chicago with a 5 day forecast" style={img}/>
+                    <img src={require('../images/weather-dashboard.jpg')} alt="a weather dashboard for Chicago with a 5 day forecast" style={img}/>
                 </div>
                 {/* module 5 */}
                 <div style={project}>
@@ -111,7 +109,7 @@ function Portfolio() {
                     </div>
                     <p>Plan your day in an hour-by-hour format.</p>
                     <p>Tools: HTML, CSS, JavaScript</p>
-                    <img src={require('../images/work-day-scheduler.png')} alt="a vertically aligned time table starting with 9am and repeating every hour until 5pm, past hours are colored gray, the current hour is colored red, and future hours are colored green" style={img}/>
+                    <img src={require('../images/work-day-scheduler.jpg')} alt="a vertically aligned time table starting with 9am and repeating every hour until 5pm, past hours are colored gray, the current hour is colored red, and future hours are colored green" style={img}/>
                 </div>
                 {/* module 4 */}
                 <div style={project}>
@@ -121,7 +119,7 @@ function Portfolio() {
                     </div>
                     <p>Test your coding knowledge with this quiz.</p>
                     <p>Tools: HTML, CSS, JavaScript</p>
-                    <img src={require('../images/code-quiz.png')} alt="text reading Coding Quiz Challenge with a blue start quiz button" style={img}/>
+                    <img src={require('../images/code-quiz.jpg')} alt="text reading Coding Quiz Challenge with a blue start quiz button" style={img}/>
                 </div>
                 {/* module 3 */}
                 <div style={project}>
@@ -131,7 +129,7 @@ function Portfolio() {
                     </div>
                     <p>Generate a uniquely random password with a few simple questions.</p>
                     <p>Tools: HTML, CSS, JavaScript</p>
-                    <img src={require('../images/password-generator.png')} alt="text box on white background with red button to generate a password" style={img}/>
+                    <img src={require('../images/password-generator.jpg')} alt="text box on white background with red button to generate a password" style={img}/>
                 </div>
                 
             </section>
