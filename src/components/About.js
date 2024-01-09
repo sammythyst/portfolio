83faacbca53c88abb6d4a55b9039a202a3a6eb94
sasw <saswain96@gmail.com>
2024-01-09 16:32:13 -0600

    naming conventions

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ function About() {
         <div className="m-5">
             <div className='d-md-flex px-4'>
                 <div>
-                    <h3>Hi, my name is Sam and I am a Front-End Web Developer.</h3>
+                    <h3>Hi, my name is Sam and I am a front-end web developer.</h3>
                     <p>My journey started in the Computer Graphics Technology program at Purdue University Northwest in the fall of 2016. I initially entered the program to follow a different path but soon found an appreciation and enjoyment for web development along the way.</p>
                     <p>Two years after graduating from Purdue, I decided to further my education after discovering a coding boot camp through Michigan State University (partnered with Trilogy Education Services, LLC). Throughout this program I was able to enhance my front-end coding skills and gain knowledge in back-end coding to be able to create full stack websites.</p>
                 </div>
